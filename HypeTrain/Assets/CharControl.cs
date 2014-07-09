51613bdbe6e1df3cc3211b290c1d6b1bd37dcfd3
fordfiler6 <ffiler@usc.edu>
2014-07-08 20:29:49 -0700

    material for wall and crate, prevents sticking, one jump per space click

--- HypeTrain/Assets/CharControl.cs
+++ HypeTrain/Assets/CharControl.cs
@@ -7,30 +7,32 @@ public class CharControl : MonoBehaviour {
 	
 	//Ground stuff
 	bool grounded = false;
+	bool hasJumped = false;
 	public Transform groundCheck;
-	float groundRadius = 0.2f;
+	float raycastLength = 0.01f;
 	public LayerMask whatIsGround;
 	public float jumpForce = 300f;
 
 	void Start() {
-
 	}
 
 	void Update () {
-		if(grounded && Input.GetKeyDown(KeyCode.Space)) {
+		if(grounded && Input.GetKey(KeyCode.Space) && !hasJumped) {
 			Debug.Log ("space pressed");
 			rigidbody2D.AddForce(new Vector2(0, jumpForce));
-
+			hasJumped = true;
+		}
+		if (!Input.GetKey(KeyCode.Space) && hasJumped) {
+			Debug.Log ("Reset jump");
+			hasJumped= false;
 		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
-
+		//grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		grounded = Physics2D.Raycast (groundCheck.position, -Vector2.up, raycastLength, whatIsGround);
 		float moveH = Input.GetAxis ("Horizontal");
 		rigidbody2D.velocity = new Vector2 (moveH * maxSpeed, rigidbody2D.velocity.y);
-
 	}
 }
