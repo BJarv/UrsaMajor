aaa688384a843e3c1c0adea91dfcf2033acc516e
BJarv <brandon.jarv@gmail.com>
2014-07-27 17:04:05 -0700

    Start of trainspawner

--- HypeTrain/Assets/TrainEnter.cs
+++ HypeTrain/Assets/TrainEnter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 
 public class TrainEnter : MonoBehaviour {
+	public GameObject trainSpawner;
+	public GameObject train;
 	
 	//private GameObject Player = null;
 	// Use this for initialization
@@ -18,12 +20,14 @@ public class TrainEnter : MonoBehaviour {
 	{
 		if(Input.GetKey(KeyCode.E)){
 			Physics2D.IgnoreCollision (hit, transform.parent.gameObject.collider2D, true);
+			trainSpawner.KillTrain(train);
 		}
 	}
 	void OnTriggerStay2D(Collider2D hit) 
 	{
 		if(Input.GetKey(KeyCode.E)){
 			Physics2D.IgnoreCollision (hit, transform.parent.gameObject.collider2D, true);
+			trainSpawner.KillTrain(train);
 		}
 	}
 	void OnTriggerExit2D(Collider2D hit) 
---
+++ HypeTrain/Assets/trainSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trainSpawner : MonoBehaviour {
+
+	private int carsCompleted;
+	public GameObject[] possTrains;
+	private Queue trains;
+	private GameObject tempTrain;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		trains = new Queue();
+		QueueAndMove();
+		QueueAndMove();
+	}
+
+	void QueueAndMove(){
+		tempTrain = Instantiate(possTrains[Random.Range(0, possTrains.GetLength(0))], transform.position, Quaternion.identity);
+		trains.Enqueue(tempTrain);
+		transform.position.x += tempTrain.carWidth();
+	}
+
+	//queue train
+	//move spawner to end of train
+	//queue train
+	//move spawner
+	//wait for player to move to 2nd train
+	//queue train
+	//move spawner
+	//wait for player to move to 3rd train
+	//queue train, move spawner, and delete first
+	//wait
+	//queue and delete
+	//repeat last 2
+
+
+	//only kill train if !(train.left.x < player.x && player.x < train.right.x)
+	void KillTrain(GameObject train) {
+		if(!(train.left.x < player.x && player.x < train.right.x)){
+			Destroy(trains.Dequeue());
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
