fcf596bcce2ca046548d2158024799896c8715b2
Sam <sefields@ucsc.edu>
2014-08-11 22:07:24 -0700

    idk

--- HypeTrain/Assets/gun.cs
+++ HypeTrain/Assets/gun.cs
@@ -28,6 +28,17 @@ public class gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//rotation
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = 5.23f;
+		
+		Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+		mousePos.x = mousePos.x - objectPos.x;
+		mousePos.y = mousePos.y - objectPos.y;
+		
+		float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 	
 		if (Input.GetButtonDown ("Fire1") && Firable ()) {
 			sTimerOn = true;
