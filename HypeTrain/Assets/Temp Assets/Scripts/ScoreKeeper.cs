79e607e5cc07943b3ad064902d6a29da1055c0fd
Hayden Platt <misterplatt17@gmail.com>
2014-08-21 09:38:30 -0700

    Updated score display

--- HypeTrain/Assets/Temp Assets/Scripts/ScoreKeeper.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/ScoreKeeper.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class ScoreKeeper : MonoBehaviour {
 
+	public GUIStyle scoreStyle;
+
 	public static int Score;
 	void Awake () 
 	{
 		Score = 0;
 	}
-	
+
 	void OnGUI()
 	{
 		GUI.color = Color.black;
-		GUI.Label (new Rect (0, 0, 125, 25), "Loot Collected: $" + Score);
+		GUI.Label (new Rect (7, 3, 500, 25), "Loot: $" + Score, scoreStyle);
 	}
 }
