51734607a06ffd92be2ee0990d086977a515a5fe
Sam <sefields@ucsc.edu>
2014-09-17 21:39:55 -0700

    Background script written, far from working

---
+++ HypeTrain/Assets/Background.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	private float red = 0.5f;
+	private float green = 0.5f;
+	private float blue = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*red = red + 0.1f;
+		if (red + .001 >= 1)
+						red = 0;
+		green = green + 0.1f;
+		if (green + .001 >= 1)
+						green = 0;
+		blue = blue + 0.1f;
+		if (blue + .001 >= 1)
+						blue = 0;
+		GetComponent(GUITexture).color = new Color(red,green,blue,0);*/
+	}
+}
