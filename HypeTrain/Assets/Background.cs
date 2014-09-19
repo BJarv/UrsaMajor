bfe84b3e992765ca220b8fc0cc0823e4af94045c
Sam <sefields@ucsc.edu>
2014-09-18 20:35:17 -0700

    Day / Night tweaked

--- HypeTrain/Assets/Background.cs
+++ HypeTrain/Assets/Background.cs
@@ -11,45 +11,38 @@ public class Background : MonoBehaviour {
 	public Color dayday = new Color(1f, 1f, 1f, 0);
 	public Color lerpedColor = new Color(.5f, .5f, 0, 1);
 	public int countSwitch = 0; // 0 = let's count up, 1 = let's count down
+	public int delay = 0;
 
 	// Use this for initialization
 	void Start () {
 
 	}
 
+	void brighten () {
+		t = t + .01f;
+		}
+
+	void darken () {
+		t = t - .01f;
+		}
+
 	void Update () {
 		//Increment t
 		if (countSwitch == 0)
-						t = t + .01f;
-		else if (countSwitch == 1)
-						t = t - .01f;
+						brighten ();
+				else if (countSwitch == 1)
+						darken ();
 
-		if (t <= 0f)
+		if (t <= 0f && delay % 1000 == 0)
 						countSwitch = 0; 
 
-		if (t >= 1f)
+		if (t >= 1f && delay % 1000 == 0)
 						countSwitch = 1;
 
-		/*Increment red
-		red = red + 0.001f;
-		if (red + .001 >= 1)
-						red = 0;
-
-		//Increment green
-		green = green + 0.001f;
-		if (green + .001 >= 1)
-						green = 0;
-
-		//Increment blue
-		blue = blue + 0.001f;
-		if (blue + .001 >= 1)
-						blue = 0;*/
-
-		//Throw red, green, blue into a new color
 		lerpedColor = Color.Lerp(dayday, nightynight, t);
 
-		//Set the GUI to that color
 		guiTexture.color = lerpedColor;
+		delay = delay + 1;
 
 		//Debug
 		//print("Red : " + red + "Green : " + green + "Blue : " + blue);
