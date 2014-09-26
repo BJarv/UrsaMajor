77d7446ecb0aac38dfc09ced5b38d3b48fd1d69d
Hayden Platt <misterplatt17@gmail.com>
2014-09-25 18:25:17 -0700

    Forgot to add my changes to the main menu

--- HypeTrain/Assets/Temp Assets/MainMenu/Scripts/MainMenu.cs
+++ HypeTrain/Assets/Temp Assets/MainMenu/Scripts/MainMenu.cs
@@ -10,12 +10,12 @@ public class MainMenu : MonoBehaviour {
 		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
 
 		//Display buttons
-		if (GUI.Button (new Rect (Screen.width * .44f, Screen.height * .5f, Screen.width * .2f, Screen.height * .1f), "Play Game")) {
+		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .34f, Screen.width * .3f, Screen.height * .1f), "Play Game")) {
 			Debug.Log ("Pressed Play!");
 			Application.LoadLevel("Alpha");
 		};
 
-		if (GUI.Button (new Rect (Screen.width * .73f, Screen.height * .5f, Screen.width * .2f, Screen.height * .1f), "Quit")) {
+		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .48f, Screen.width * .3f, Screen.height * .1f), "Quit")) {
 			Debug.Log ("Pressed Quit!");	
 			Application.Quit ();
 		};
