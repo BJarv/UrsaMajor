ab0f51ae1abdb0573162d4fd5a71bf52f43423be
Hayden Platt <misterplatt17@gmail.com>
2014-09-25 19:03:09 -0700

    Added tutorial/lumber car sketches, added Tutorial Car scene

--- HypeTrain/Assets/Temp Assets/MainMenu/Scripts/MainMenu.cs
+++ HypeTrain/Assets/Temp Assets/MainMenu/Scripts/MainMenu.cs
@@ -12,20 +12,12 @@ public class MainMenu : MonoBehaviour {
 		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
 
 		//Display buttons
-<<<<<<< HEAD
-		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .34f, Screen.width * .3f, Screen.height * .1f), "Play Game")) {
-=======
 		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .335f, Screen.width * .3f, Screen.height * .12f), playButton, GUIStyle.none)) {
->>>>>>> 7421647b6276be0afd95b2185c01cab148a3b228
 			Debug.Log ("Pressed Play!");
 			Application.LoadLevel("entrances");
 		};
 
-<<<<<<< HEAD
-		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .48f, Screen.width * .3f, Screen.height * .1f), "Quit")) {
-=======
 		if (GUI.Button (new Rect (Screen.width * .35f, Screen.height * .48f, Screen.width * .3f, Screen.height * .12f), playButton, GUIStyle.none)) {
->>>>>>> 7421647b6276be0afd95b2185c01cab148a3b228
 			Debug.Log ("Pressed Quit!");	
 			Application.Quit ();
 		};
