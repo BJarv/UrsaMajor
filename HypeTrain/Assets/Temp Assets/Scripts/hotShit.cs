fa1841732f441d641d844745de4cea9f6163fad3
Hayden Platt <misterplatt17@gmail.com>
2014-11-09 14:44:10 -0800

    Updated Freezer, Tutorial, Coal, enemies and loot

---
+++ HypeTrain/Assets/Temp Assets/Scripts/hotShit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hotShit : MonoBehaviour {
+
+	private GameObject player = null;
+	public float knockUpForce;
+
+	void Start () {
+		player = GameObject.Find("character");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//When character lands on this surface, deal damage, apply knockup and hitAnim
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "character") {
+			col.gameObject.GetComponent<PlayerHealth>().Hurt(10);
+			col.gameObject.rigidbody2D.AddForce(new Vector2(0, knockUpForce));	
+			player.GetComponent<CharControl>().hitAnim();
+		}
+	}
+	void OnCollisionStay2D(Collision2D col) {
+		if (col.gameObject.name == "character") {
+			col.gameObject.GetComponent<PlayerHealth>().Hurt(10);
+			col.gameObject.rigidbody2D.AddForce(new Vector2(0, knockUpForce));	
+			player.GetComponent<CharControl>().hitAnim();	
+		}
+	}
+}
