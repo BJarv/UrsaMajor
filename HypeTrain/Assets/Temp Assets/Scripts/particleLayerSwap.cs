fb8ecc17d2783df5217d7c7f4e18181a69e61982
BJarv <brandon.jarv@gmail.com>
2015-01-02 01:01:20 -0800

    particle effects script fix

--- HypeTrain/Assets/Temp Assets/Scripts/particleLayerSwap.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/particleLayerSwap.cs
@@ -3,10 +3,25 @@ using System.Collections;
 
 public class particleLayerSwap : MonoBehaviour {
 
+	public int sortLayer = 2; //1 is rendered in front of all sprites on particles layer, 2 is rendered on obstacles layer, 3 is rendered behind train
+	public bool renderForward = false; //false for setting effect behind set layer, true for effect in front of set layer
 	// Use this for initialization
 	void Start () {
-		particleSystem.renderer.sortingLayerName = "Default";
-		particleSystem.renderer.sortingOrder = 1;
+		if (renderForward) {
+			particleSystem.renderer.sortingOrder = 1;
+		} else {
+			particleSystem.renderer.sortingOrder = -1;
+		}
+		if (sortLayer == 1){
+			gameObject.GetComponent<ParticleSystem>().renderer.sortingLayerName = "Particles";
+		} else if (sortLayer == 2) {
+			gameObject.GetComponent<ParticleSystem>().renderer.sortingLayerName = "Obstacles";
+		} else if (sortLayer == 3){
+			gameObject.GetComponent<ParticleSystem>().renderer.sortingLayerName = "Default";
+		} else {
+			Debug.Log ("bad input for sortLayer");
+		}
+
 	}
 
 	
