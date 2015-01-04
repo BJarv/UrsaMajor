acaad332e9b0738cb28802daa7d540f4b8c125c8
BJarv <brandon.jarv@gmail.com>
2015-01-04 01:35:04 -0800

    Shop car loot display and bullet UI fix

---
+++ HypeTrain/Assets/Temp Assets/Scripts/LifeLootDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LifeLootDisplay : MonoBehaviour {
+
+	Text total;
+
+
+	// Use this for initialization
+	void Start () {
+		total = transform.Find ("amount").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		total.text = "$" + Game.lifetimeLoot;
+	}
+}
