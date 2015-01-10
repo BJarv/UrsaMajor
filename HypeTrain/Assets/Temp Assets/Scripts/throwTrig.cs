088ccec44e777eee6e75d7566d94b893c184037a
BJarv <brandon.jarv@gmail.com>
2015-01-09 21:34:43 -0800

    dino throw, WTF IS WITH IT TURNING

---
+++ HypeTrain/Assets/Temp Assets/Scripts/throwTrig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throwTrig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D hit) {
+		Debug.Log (hit.tag);
+ 		if(hit.tag == "Player") {
+			transform.parent.GetComponent<BigDino>().inThrowRange = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D hit) {
+		Debug.Log (hit.tag);
+		if(hit.tag == "Player") {
+			transform.parent.GetComponent<BigDino>().inThrowRange = false;
+		}
+	}
+}
