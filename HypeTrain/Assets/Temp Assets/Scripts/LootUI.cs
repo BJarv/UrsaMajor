e46ad996393f87397fb3ce0526a6c504a6952ae0
BJarv <brandon.jarv@gmail.com>
2015-01-11 15:39:15 -0800

    loot display

---
+++ HypeTrain/Assets/Temp Assets/Scripts/LootUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LootUI : MonoBehaviour {
+	private Text loot;
+	// Use this for initialization
+	void Start () {
+		loot = transform.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		loot.text = "$" + ScoreKeeper.Score;
+	}
+}
--- HypeTrain/Assets/Temp Assets/Scripts/trailToggle.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/trailToggle.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class trailToggle : MonoBehaviour {
 
 	public float trailLength = 2;
-	LineRenderer trail;
+	TrailRenderer trail;
 
 	void Start () {
 		//Grab the linerenderer from the object and default it to disabled
-		trail = gameObject.GetComponent<LineRenderer>();
+		trail = gameObject.GetComponent<TrailRenderer>();
 		trail.enabled = false;
 		trail.sortingLayerName = "Player";
 		trail.sortingOrder = 1;
