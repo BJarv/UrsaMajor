36e4cf119e0c215fef4e48397a744a0813d4249a
Hayden Platt <misterplatt17@gmail.com>
2015-01-10 20:06:11 -0800

    Took some screenshots

--- HypeTrain/Assets/Temp Assets/Scripts/trailToggle.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/trailToggle.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class trailToggle : MonoBehaviour {
 
 	public float trailLength = 2;
+	LineRenderer trail;
+
+	void Start () {
+		//Grab the linerenderer from the object and default it to disabled
+		trail = gameObject.GetComponent<LineRenderer>();
+		trail.enabled = false;
+		trail.sortingLayerName = "Player";
+		trail.sortingOrder = 1;
+	}
 
 	public void On() {
 		gameObject.GetComponent<TrailRenderer> ().enabled = true;
