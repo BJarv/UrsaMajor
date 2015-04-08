a01000bac144f86653cc9f9c23db027b0621a5b4
BJarv <brandon.jarv@gmail.com>
2015-04-07 18:57:50 -0700

    train camera shake with alt to call for other events

--- HypeTrain/Assets/Temp Assets/Scripts/CameraShake.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/CameraShake.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 
 public class CameraShake : MonoBehaviour {
-
+	
 	//Currently makes use of having 2 objects above the camera. 1 to keep a stationary position to return to, and 1 to transform the
 	//localposition of. The camera can then be moved freely without any worry of the camera bump affecting it.
-
+	
 	bool shake = false;
+	bool altShake = false;
 	public float shakeTime = 0.2f;
-
-	public float shakeAmount = 0.5f;
-
+	
+	public float shakeAmount = 0.15f;
+	public float altShakeAmount = 2f;
+	
 	public bool test = false;
-
+	
 	void Start() {
 		StartCoroutine(trainShake());
 	}
@@ -21,33 +23,44 @@ public class CameraShake : MonoBehaviour {
 	void Update () {
 		if(test) {
 			test = false;
-			bumpIt ();
+			bumpIt(altShakeAmount);
 		}
-		if (shake) {
-			transform.localPosition = Vector3.zero + Random.insideUnitSphere * shakeAmount;
+		if (altShake){
+			transform.localPosition = Vector3.zero + Random.insideUnitSphere * altShakeAmount;
+		} else if (shake) {
+			transform.localPosition = Vector3.zero + Random.insideUnitSphere * shakeAmount; 
 		} else {
 			transform.localPosition = Vector3.zero;
 		}
+		
 	}
-
-	public void bumpIt() {
+	
+	
+	public void bumpIt(float intensity = 2f, float time = .1f) {
+		altShakeAmount = intensity;
+		altShake = true;
+		StartCoroutine(altShakeOff (time));
+	}
+	
+	void trainBumpIt() {
 		shake = true;
 		StartCoroutine(shakeOff (shakeTime));
 	}
-
-	public void bumpIt(float time) {
-		shake = true;
-		StartCoroutine(shakeOff (time));
-	}
-
+	
 	IEnumerator shakeOff(float time) {
 		yield return new WaitForSeconds(time);
 		shake = false;
 	}
-
-	IEnumerator trainShake(){
-		InvokeRepeating("bumpIt", 0f, 1.2f);
-		yield return new WaitForSeconds(0.2f);
-		InvokeRepeating ("bumpIt", 0f, 1.2f);
+	
+	IEnumerator altShakeOff(float time) {
+		yield return new WaitForSeconds(time);
+		altShake = false;
 	}
+	
+	IEnumerator trainShake(){
+		InvokeRepeating("trainBumpIt", 0f, 1.2f);
+		yield return new WaitForSeconds(0.2f);
+		InvokeRepeating ("trainBumpIt", 0f, 1.2f);
+	}
+	
 }
