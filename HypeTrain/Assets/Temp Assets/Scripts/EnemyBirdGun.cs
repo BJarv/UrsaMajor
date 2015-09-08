1762d081b5df9c51501d3f73eec818dd3df12320
Hayden Platt <misterplatt17@gmail.com>
2015-09-08 16:27:58 -0700

    Barely started Tucker Car boss guy

---
+++ HypeTrain/Assets/Temp Assets/Scripts/EnemyBirdGun.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBirdGun : MonoBehaviour {
+	
+	private bool shooting = false;
+	private int direction = 0;
+	public float bulletSpeed = 5000f;
+	public GameObject bullet;
+	Transform player;
+	bool shootable = true; //currently able to shoot
+	public float shootCD = 1f;
+	
+	public GameObject shotParticles;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("character").transform;
+		shootCD *= Multiplier.enemyShootCD;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shooting) { //if true, enemy has aggro'd and shooting at player
+			if (shootable) {
+				shootable = false;
+				Invoke ("shootOn", shootCD);
+				shootBird ();
+			}
+		}
+	}
+	
+	void shootOn() {
+		shootable = true;
+	}
+	
+	GameObject shootBird(){ //90 for straight forward
+		//Quaternion rotation = Quaternion.LookRotation(player.position);
+		Vector3 playerPos = player.transform.position;
+		Quaternion rotToPlayer = Quaternion.FromToRotation(Vector3.up, playerPos - transform.position);
+		rotToPlayer.Set (rotToPlayer.x, rotToPlayer.y, rotToPlayer.z, rotToPlayer.w);
+		GameObject bulletInstance = Instantiate(bullet, transform.position, rotToPlayer) as GameObject;
+		bulletInstance.GetComponent<Rigidbody2D>().AddForce(bulletInstance.transform.up * bulletSpeed);
+		
+		rotToPlayer *= Quaternion.Euler (0, 0, 90);
+		GameObject particles = (GameObject)Instantiate(shotParticles, transform.position, rotToPlayer); //apply particles
+		particles.GetComponent<ParticleSystem>().Play ();
+		Destroy (particles, particles.GetComponent<ParticleSystem>().startLifetime);
+		
+		return bulletInstance;
+	}
+	
+	public void isShooting(bool x, int y){
+		shooting = x;
+		direction = y;
+	}
+}
+
