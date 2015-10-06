f7fa24d365e4b3beeef70b8464be09ec9c50b166
Hayden Platt <misterplatt17@gmail.com>
2015-10-06 12:13:16 -0700

    Check

--- HypeTrain/Assets/Temp Assets/Scripts/HYPEController.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/HYPEController.cs
@@ -23,7 +23,7 @@ public class HYPEController : MonoBehaviour {
 	public AudioClip HYPEsound;
 
 	//Default HYPE value
-	public static string HYPEMode = "orange";
+	public static string HYPEMode = "blue";
 
 	public static bool lazers = false;
 	public static bool airblasts = false;
