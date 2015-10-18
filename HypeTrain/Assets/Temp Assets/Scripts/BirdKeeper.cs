6a09a0d8d358f3df2ab6f11ab1c3e6b4788a0de3
Sam <sefields@ucsc.edu>
2015-10-18 13:55:26 -0700

    Birdkeeper now goes into low health mode

--- HypeTrain/Assets/Temp Assets/Scripts/BirdKeeper.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/BirdKeeper.cs
@@ -9,10 +9,12 @@ public class BirdKeeper : ShootingEnemy {
 	private EnemyLobber lobber;
 	private Animator anim;
 	private bool dying;
+	private ParticleSystem sparks;
 
 	override protected void Start () {  //overrides start function of enemy.cs
 		anim = GetComponent<Animator> ();
 		base.Start ();
+		sparks = transform.Find ("sparks").GetComponent<ParticleSystem> ();
 		try {
 			gun = transform.Find ("enemyGun").GetComponent<EnemyGun>();
 		} catch {
@@ -69,6 +71,7 @@ public class BirdKeeper : ShootingEnemy {
 		health -= damage;
 		if (health <= 30 && dying == false) {
 			dying = true;
+			sparks.Play();
 			anim.SetTrigger ("dying");
 		}
 		if (health <= 0) {
