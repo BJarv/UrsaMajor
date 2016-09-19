3a475bcac4ddb4f241c1dfa1b870a54d5df90286
Sam <sam.fields94@gmail.com>
2016-09-18 23:33:23 -0700

    Implemented crate particle fx (but they are not visible in Game view yet)

--- HypeTrain/Assets/Temp Assets/Scripts/breakable.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/breakable.cs
@@ -1,30 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class breakable : MonoBehaviour {
+public class breakable : MonoBehaviour
+{
 
 	public float durability = 1f;
 	public bool glass = false;
 	public bool dropCash = false;
 	public GameObject requiredProjectile = null;
 
+	public GameObject breakParticle;
 	public Animator breakAnimator;
 	public AudioClip breakSound;
 
 
-	[HideInInspector] public Itemizer money; 
+	[HideInInspector] public Itemizer money;
 	// Use this for initialization
-	void Start () {
-		money = GameObject.Find ("Main Camera").GetComponent<Itemizer>();
+	void Start ()
+	{
+		money = GameObject.Find ("Main Camera").GetComponent<Itemizer> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
 	//Deal damage if there is no specific projectile or projectile matches required one
-	public void Damage(GameObject projectile){
+	public void Damage (GameObject projectile)
+	{
 		if (requiredProjectile == null) {
 			durability--; //subtract one point from durability on hit
 		} else if (projectile.name == requiredProjectile.name + "(Clone)") {
@@ -33,28 +38,38 @@ public class breakable : MonoBehaviour {
 
 		//If durability is zero, play animation if it exists, drop cash, and destroy object
 		if (durability <= 0) {
-			if (breakAnimator != null){ //Do this if animator exists
-				if (breakSound != null){ 
-					AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+			if (breakAnimator != null) { //Do this if animator exists
+				if (breakSound != null) { 
+					AudioSource.PlayClipAtPoint (breakSound, Camera.main.transform.position);
 				}
-				if (dropCash){ //Drop cash if true
-					money.At(new Vector3(transform.position.x, transform.position.y - 2, transform.position.z), Random.Range ((int)(1 * Multiplier.moneyDrop),(int)(5 * Multiplier.moneyDrop)));
+				if (dropCash) { //Drop cash if true
+					money.At (new Vector3 (transform.position.x, transform.position.y - 2, transform.position.z), Random.Range ((int)(1 * Multiplier.moneyDrop), (int)(5 * Multiplier.moneyDrop)));
 				}
 				breakAnimator.Play ("Bin_Opening");
-				gameObject.GetComponent<Collider2D>().enabled = false;
+				gameObject.GetComponent<Collider2D> ().enabled = false;
 				dropCash = false;
 			} else {  //Do this if there is no animator to play
-				if (dropCash){ //Drop cash if true
-					money.At(transform.position, Random.Range ((int)(1 * Multiplier.moneyDrop),(int)(5 * Multiplier.moneyDrop)));
+				if (dropCash) { //Drop cash if true
+					money.At (transform.position, Random.Range ((int)(1 * Multiplier.moneyDrop), (int)(5 * Multiplier.moneyDrop)));
 				}
-				Destroy (gameObject);
+				Break ();
 			}
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D colObj) {
-		if(colObj.tag == "Player" && glass && colObj.GetComponent<Rigidbody2D>().velocity.x > 15){
-			Destroy (gameObject);
+	void OnTriggerEnter2D (Collider2D colObj)
+	{
+		if (colObj.tag == "Player" && glass && colObj.GetComponent<Rigidbody2D> ().velocity.x > 15) {
+			Break ();
 		}
+	}
+
+	void Break ()
+	{
+		if (breakParticle != null) {
+			Instantiate (breakParticle, transform.position, Quaternion.identity);
+			Debug.Log ("particles were spawned");
+		}
+		Destroy (gameObject);
 	}
 }
