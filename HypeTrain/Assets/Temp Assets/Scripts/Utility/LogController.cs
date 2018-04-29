aa97c1add4f97c974c4c99e029d3812d77fbdf5c
Desktop\Platt <misterplatt17@gmail.com>
2018-04-29 13:15:49 -0700

    [LogController] Log() now automatically appends "[SCRIPT_NAME.cs]" to the message for easier trackdowns of where the log is coming from

--- HypeTrain/Assets/Temp Assets/Scripts/Utility/LogController.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/Utility/LogController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class LogController : MonoBehaviour {
 
-    [SerializeField]
-    private bool loggingEnabled = false;
+    //Flag for whether or not log messages for the given script should be output.
+    [SerializeField] private bool loggingEnabled = false;
 
+    //Basic message print, which appends "[SCRIPT_NAME.cs]" for easier trackdowns
     protected void Log(string message) {
-        if (loggingEnabled) Debug.Log(message);
+        if (loggingEnabled) Debug.Log(message + " [" + GetComponent<MonoBehaviour>().GetType().Name + ".cs]");
     }
 
 	// Use this for initialization
