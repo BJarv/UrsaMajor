326037008a8a13841547c5fa6569feb8e894621d
Hayden Platt <misterplatt17@gmail.com>
2018-11-25 11:46:52 -0800

    [Project] Updated to latest unity version
    [Breakable.cs]
    * Consolidated FX and loot drop calls into the `Break()` function.
    * Used to have essentially custom handling for specifically the Luggage bins (looked specifically for `Bin_Opening` anim state). Renamed the anim state on the Bin's controller to `OnBreak`, so moving forward all states that should be called when broken should be named `OnBreak`.
    * Added Tooltips
    * Removed `GameObject.Find()` calls
    
    [OverheadBin.controller]
    Moved out of sprites folder and into animation controller folder

--- HypeTrain/Assets/Temp Assets/Scripts/Utility/Breakable.cs
+++ HypeTrain/Assets/Temp Assets/Scripts/Utility/Breakable.cs
@@ -3,72 +3,83 @@ using System.Collections;
 
 public class Breakable : LogController
 {
+    [Tooltip("How many hits the breakable can take before breaking.")]
+    public float durability = 1f;
 
-	public float durability = 1f;
-	public bool glass = false;
-	public bool dropCash = false;
-	public GameObject requiredProjectile = null;
+    [Tooltip("If true, the player colliding with the object at a fast enough speed will cause it to break.")]
+    public bool glass = false;
 
-	public GameObject breakParticle;
-	public Animator breakAnimator;
-	public AudioClip breakSound;
+    [Tooltip("Should the breakable drop cash when destroyed?")]
+    public bool dropCash = false;
 
+    [Tooltip("If specified, the breakable will only take damage from the specified projectile.")]
+    public GameObject requiredProjectile = null;
 
-	[HideInInspector] public Itemizer money;
+    //Break effects
+    public AudioClip hitSFX;
+    public AudioClip breakSFX;
+    public GameObject breakParticles;
+    public Animator breakAnimator;
+
+    //Used for spawning cash
+    [HideInInspector] public Itemizer money;
+
 	// Use this for initialization
 	void Start ()
 	{
-		money = GameObject.Find ("Main Camera").GetComponent<Itemizer> ();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	
+		money = Camera.main.transform.GetComponent<Itemizer> ();
 	}
 
-	//Deal damage if there is no specific projectile or projectile matches required one
-	public void Damage (GameObject projectile)
-	{
-		if (requiredProjectile == null) {
-			durability--; //subtract one point from durability on hit
-		} else if (projectile.name == requiredProjectile.name + "(Clone)") {
-			durability--;
-		}
+    public void Damage(GameObject projectile)
+    {
+        //Handle durability changes
+        if (requiredProjectile == null)
+        {
+            durability--;
+        }
+        else if (projectile.name == requiredProjectile.name + "(Clone)")
+        {
+            durability--;
+        }
 
-		//If durability is zero, play animation if it exists, drop cash, and destroy object
-		if (durability <= 0) {
-			if (breakAnimator != null) { //Do this if animator exists
-				if (breakSound != null) { 
-					AudioSource.PlayClipAtPoint (breakSound, Camera.main.transform.position);
-				}
-				if (dropCash) { //Drop cash if true
-					money.At (new Vector3 (transform.position.x, transform.position.y - 2, transform.position.z), Random.Range ((int)(1 * Multiplier.moneyDrop), (int)(5 * Multiplier.moneyDrop)));
-				}
-				breakAnimator.Play ("Bin_Opening");
-				gameObject.GetComponent<Collider2D> ().enabled = false;
-				dropCash = false;
-			} else {  //Do this if there is no animator to play
-				if (dropCash) { //Drop cash if true
-					money.At (transform.position, Random.Range ((int)(1 * Multiplier.moneyDrop), (int)(5 * Multiplier.moneyDrop)));
-				}
-				Break ();
-			}
-		}
+        //Call Break() if object is out of durability
+        if (durability > 0)
+        {
+            //Play break sound
+            if (hitSFX != null) AudioSource.PlayClipAtPoint(hitSFX, transform.position);
+        }
+        else if (durability <= 0)
+        {
+            Break();
+        }
 	}
 
 	void OnTriggerEnter2D (Collider2D colObj)
 	{
-		if (colObj.tag == "Player" && glass && colObj.GetComponent<Rigidbody2D> ().velocity.x > 15) {
-			Break ();
-		}
+        //If player smashes into the "glass" object, call Break()
+		if (glass && colObj.tag == "Player" && colObj.GetComponent<Rigidbody2D> ().velocity.x > 15) Break ();
 	}
 
+    //Handles Break FX, loot drop, and destruction
 	void Break ()
 	{
-		if (breakParticle != null) {
-			Instantiate (breakParticle, transform.position, Quaternion.identity);
-		}
-		Destroy (gameObject);
+        //Play break sound
+        if (breakSFX != null) AudioSource.PlayClipAtPoint(breakSFX, transform.position);
+
+        //Play break particles
+        if (breakParticles != null) Instantiate(breakParticles, transform.position, Quaternion.identity);
+
+        //Drop cash
+        if (dropCash) money.At(transform.position, Random.Range((int)(1 * Multiplier.moneyDrop), (int)(5 * Multiplier.moneyDrop)));
+
+        //If there's an animator, use its "OnBreak" animation for destruction
+        if (breakAnimator != null) {
+            breakAnimator.Play("OnBreak");
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            dropCash = false;
+        }
+        else {
+            Destroy(gameObject);
+        }
 	}
 }
